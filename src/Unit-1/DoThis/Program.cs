d19c75a2f4f769671f2648a14f6794e175bb2807
rcps10-YL <97593879+rcps10-YL@users.noreply.github.com>
2022-02-15 14:48:39 +0000

    Unit 1 -- lesson 3 -- Props and IActorRefs

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -12,17 +12,15 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            // time to make your first actors!
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            var consoleWriterActor = MyActorSystem.ActorOf(
-                Props.Create(() => new ConsoleWriterActor()),
-                "consoleWriterActor"
-            );
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-            var consoleReaderActor = MyActorSystem.ActorOf(
-                Props.Create(() => new ConsoleReaderActor(consoleWriterActor)),
-                "consoleReaderActor"
-            );
+            // set up actors, using props (split props onto own line so easier to read)
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+            Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
             // tell console reader to begin
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
