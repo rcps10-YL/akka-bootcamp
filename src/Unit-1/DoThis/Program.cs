52e200a9544c87a1cdeaddc5a999660499a71557
rcps10-YL <97593879+rcps10-YL@users.noreply.github.com>
2022-02-15 19:47:15 +0000

    Unit 1 -- lesson 4 -- Child Actors, Actor Hierarchies and Supervision

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -9,20 +9,24 @@ namespace WinTail
 
         static void Main(string[] args)
         {
-            // initialize MyActorSystem
+            // make actor system 
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            // set up actors, using props (split props onto own line so easier to read)
+            // create top-level actors within the actor system
             Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
             IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
-            Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
-            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+
+            Props fileValidatorActorProps = Props.Create(() =>
+                new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
 
             Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
             IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
-            // tell console reader to begin
+            // begin processing
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
