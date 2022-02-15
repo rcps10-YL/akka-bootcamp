6dbc26083208ea0ba8dfcdf07eef197363c6a00f
rcps10-YL <97593879+rcps10-YL@users.noreply.github.com>
2022-02-15 10:51:28 +0000

    Unit 1 -- lesson 2 -- Defining and Handling Messages

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,4 +1,3 @@
-﻿using System;
 ﻿using Akka.Actor;
 
 namespace WinTail
@@ -13,8 +12,6 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
-
             // time to make your first actors!
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
             var consoleWriterActor = MyActorSystem.ActorOf(
@@ -27,29 +24,13 @@ namespace WinTail
                 "consoleReaderActor"
             );
 
-
             // tell console reader to begin
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
     #endregion
 }
