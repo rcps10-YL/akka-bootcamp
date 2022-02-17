b46fb0c8089ae2b933a8c2411c44904b388a2ab6
rcps10-YL <97593879+rcps10-YL@users.noreply.github.com>
2022-02-17 16:02:35 +0000

    Unit 2 -- lesson 5 -- Using Stash to Defer Processing of Messages

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -7,7 +7,7 @@ using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : ReceiveActor
+    public class ChartingActor : ReceiveActor, IWithUnboundedStash
     {
         /// <summary>
         /// Maximum number of points we will allow in a series
@@ -18,6 +18,8 @@ namespace ChartApp.Actors
         /// Incrementing counter we use to plot along the X-axis
         /// </summary>
         private int xPosCounter = 0;
+
+        public IStash Stash { get; set; }
 
         #region Messages
 
@@ -101,11 +103,18 @@ namespace ChartApp.Actors
 
         private void Paused()
         {
+            // while paused, we need to stash AddSeries & RemoveSeries messages
+            Receive<AddSeries>(addSeries => Stash.Stash());
+            Receive<RemoveSeries>(removeSeries => Stash.Stash());
             Receive<Metric>(metric => HandleMetricsPaused(metric));
             Receive<TogglePause>(pause =>
             {
                 SetPauseButtonText(false);
                 UnbecomeStacked();
+
+                // ChartingActor is leaving the Paused state, put messages back
+                // into mailbox for processing under new behavior
+                Stash.UnstashAll();
             });
         }
 
